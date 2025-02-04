120ac9f81a4e4526ab10898fd6e1d0627e89b6e2
Ryolim <valeryoks55@gmail.com>
2025-02-04 16:26:59 +0700

    Getting started with compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dep.basiccodelab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dep.basiccodelab"
         minSdk = 35
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/dep/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/dep/basiccodelab/MainActivity.kt
@@ -37,7 +37,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun GreetingPreview() {
     BasicCodeLabTheme {
